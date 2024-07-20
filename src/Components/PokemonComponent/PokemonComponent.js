775dc89476b12092504ac446e92b9cd245fe7d23
Lucky Estrada <estrada.lnp@gmail.com>
2024-07-21 00:11:21 +0800

    highscore feature,fixed bugs

--- src/Components/PokemonComponent/PokemonComponent.js
+++ src/Components/PokemonComponent/PokemonComponent.js
@@ -10,10 +10,25 @@ function PokemonComponent() {
     const [isPreloaderLoading, setPreloaderLoading] = useState(false);
     const [score, setScore] = useState(0);
     const [isShowingWeight, setShowWeight] = useState(false);
+    const [isIncorrect, setIncorrect] = useState(false);
+    const [highscore, setHighScore] = useState(0);
+    const [isDisabledButtons, setDisableButtons] = useState(false);
  
     //get random number that will be placed inside the api link
     const randomNumGenerator = () => {
         return Math.floor(Math.random() * 1026);
+    }
+
+    const resetGame = () => {
+        setPrevPokemon(null);
+        setNewPokemon(null);
+        setLoading(true);
+        setPreloaderLoading(false);
+        setShowWeight(false);
+        setIncorrect(false)
+        setDisableButtons(false)
+        setScore(0);
+        localStorage.setItem("highscore", highscore);
     }
 
 
@@ -45,6 +60,7 @@ function PokemonComponent() {
             .then(() => {
                 
                 setLoading(false);
+                setDisableButtons(false)
             })
         } else {
             
@@ -54,6 +70,7 @@ function PokemonComponent() {
                 setNewPokemon(response1.data);
                 console.log(response1);
                 setPrevPokemon(NewPokemon);
+                setDisableButtons(false)
             })
             .catch((error) => {
                 console.log(error);
@@ -62,13 +79,28 @@ function PokemonComponent() {
     }
 
     useEffect(() => {
+        const storedHighScore = JSON.parse(localStorage.getItem("highscore"));
+        if(storedHighScore) {
+            setHighScore(storedHighScore);
+        } else {
+            setHighScore(0);
+        }
+    }, [])
+
+    useEffect(() => {
         
             
         setPreloaderLoading(false)
         
     }, [NewPokemon]);
 
-   
+    //check if score is bigger than highscore
+   useEffect(() => {
+    if (score > highscore) {
+        setHighScore(score);
+        localStorage.setItem("highscore", highscore);
+    }
+   }, [score])
 
     const handlePokemonWeightChecker = (prevPokemon, newPokemon, userInput) => {
         console.log(prevPokemon);
@@ -77,7 +109,7 @@ function PokemonComponent() {
         if(prevPokemon < newPokemon && userInput == 'Lighter') {
             console.log("correct")
             setScore(score + 1);
-
+            setDisableButtons(true);
             setShowWeight(true);
 
 
@@ -85,8 +117,11 @@ function PokemonComponent() {
             console.log("correct");
             setScore(score + 1);
             setShowWeight(true);
+            setDisableButtons(true);
         } else {
             console.log("incorrect");
+            setIncorrect(true);
+            setDisableButtons(true);
         }
     }
 
@@ -110,13 +145,23 @@ function PokemonComponent() {
                         </div>
 
                         <div className='text-white flex flex-col items-center justify-center p-5'>
+                            {!isDisabledButtons ? 
+                            <>
                             <h2><span className='capitalize'>{PrevPokemon.name}</span> is</h2>
                             <div className='flex flex-col'>
                                 <button onClick={(e) => handlePokemonWeightChecker(PrevPokemon.weight, NewPokemon.weight, e.target.innerText)} className='px-[2rem] py-2 rounded-xl border-white border-2 my-5'>Lighter</button>
                                 <button onClick={(e) => handlePokemonWeightChecker(PrevPokemon.weight, NewPokemon.weight, e.target.innerText)} className='px-[2rem] py-2 rounded-xl border-white border-2 my-5'>Heavier</button>
                             </div>
-                            <h2>than <span className='capitalize'>{NewPokemon.name}</span></h2>
-                            {isShowingWeight && <button onClick={handleFetchPokemon} className='text-black px-4 bg-green-400 py-3 rounded-md mt-10'>Continue</button>}
+                            <h2>than <span className='capitalize'>{NewPokemon.name}</span></h2> 
+                            </>
+                            :
+                            <>
+                            {(isShowingWeight && !isIncorrect) && <button onClick={handleFetchPokemon} className='text-black px-4 bg-green-400 py-3 rounded-md mt-10'>Continue</button>}
+                            {isIncorrect && <button onClick={resetGame} className='text-black px-4 bg-red-400 py-3 rounded-md mt-10'>Try Again!</button>}
+                            </>
+                        }
+                           
+                           
                         </div>
 
 
@@ -126,13 +171,14 @@ function PokemonComponent() {
                             <h1 className='text-white text-3xl capitalize font-bold'>{NewPokemon.name}</h1>
                         </div>
                     </div>
-                    <h1 className='text-white'>Score: {score}</h1>
+                    <h1 className='text-white mt-4'>Score: {score}</h1>
                 </div>) 
                 : 
                 <div className='flex items-center justify-center h-[100vh] flex-col'>
                 <img className='w-[30rem] h-[auto]' src={logo}></img>
                 <h1 className='text-white text-5xl sm:text-[4rem] font-bold '>Lighter or Heavier!</h1>
-                <button onClick={handleFetchPokemon} className='px-4 bg-green-400 py-3 rounded-md mt-10'>Start Game</button>
+                <button onClick={handleFetchPokemon} className='px-4 bg-green-400 py-3 rounded-md my-10'>Start Game</button>
+                <p className='text-white '>Your highscore is: {highscore}</p>
             </div>
             }
 
